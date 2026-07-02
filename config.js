bcf2e03760d0b166712426d253376b89ce60fadf
jergan-studio <jose343lopez22@gmail.com>
2026-07-02 14:59:59 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,12 @@
-const mode = survival
+/**
+ * JergServers Configuration Matrix
+ */
+const Config = {
+    // Active game mode settings: 'survival' or 'creative'
+    mode: 'survival',
+    
+    // Anti-Knockoff enforcement toggle (Disconnects unverified clients)
+    allowKnockoffs: false 
+};
+
+module.exports = Config;
